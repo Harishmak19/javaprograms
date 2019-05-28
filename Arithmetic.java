26794c28a71c58d495282abf0f04ac2c6c05518d
Harishmak19 <50789131+Harishmak19@users.noreply.github.com>
2019-05-28 12:22:18 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,14 @@
+class Arithmetic
+{
+public static void main(String args[])
+{
+int a=568,x,y,z;
+x=a/10;
+z=a%10;
+y=x/10;
+x=x%10;
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+}
+}
