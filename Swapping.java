3250be10f07f4c15f07fb45f92033ba072d6286b
Harishmak19 <50789131+Harishmak19@users.noreply.github.com>
2019-05-28 12:56:26 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,16 @@
+class Swapping
+{
+public static void main(String args[])
+{
+int num1=2,num2=4,num3=6,num4=8,temp;
+temp=num4;
+num4=num3;
+num3=num2;
+num2=num1;
+num1=temp;
+System.out.println(num1);
+System.out.println(num2);
+System.out.println(num3);
+System.out.println(num4);
+}
+}
