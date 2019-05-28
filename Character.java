5efbb4fd19321f22d0737032c55d1568fa33e512
Harishmak19 <50789131+Harishmak19@users.noreply.github.com>
2019-05-28 12:39:48 +0530

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,15 @@
+class Character
+{
+public static void main(String args[])
+{
+char alpha = 'a';
+alpha++;
+System.out.println(alpha);
+alpha++;
+System.out.println(alpha);
+alpha++;
+System.out.println(alpha);
+alpha++;
+System.out.println(alpha);
+}
+}
