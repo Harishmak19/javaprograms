ab5cd56209273585f09f0b4da9a37756e5c3b5a4
Harishmak19 <50789131+Harishmak19@users.noreply.github.com>
2019-05-31 18:01:24 +0530

    Add files via upload

---
+++ SmallestofThreeNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class SmallestofThreeNumbers
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the first number: ");
+int a=sc.nextInt();
+System.out.println("Enter the second number: ");
+int b=sc.nextInt();
+System.out.println("Enter the third number: ");
+int c=sc.nextInt();
+if(a<b && a<c){
+System.out.println(a+" is smallest number ");
+}
+else if(b<c && b<a){
+System.out.println(b+" is smallest number ");
+}
+else{
+System.out.println(c+" is smallest number ");
+}
+}}
